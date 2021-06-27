27b4833c6bf8baeb05e82c9a2aeb12a0b8d72f7e
Cristian <31905715+Cristian1t@users.noreply.github.com>
2021-06-27 03:03:10 +0200

    rest of components

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -10,13 +10,19 @@ import { Task } from 'src/app/Task';
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
   faTimes = faTimes;
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  onDelete(task) {
+  onDelete(task: Task) {
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 }
